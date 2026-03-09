6a840cc9d25834fba76cb0a287a42ef72a3d4f82
Farid Rezaei <frd.rezaii@gmail.com>
2026-03-09 21:17:28 +0100

    updated the logic for accepted payments

--- models/marts/facts_orders.sql
+++ models/marts/facts_orders.sql
@@ -5,4 +5,5 @@ SELECT
 FROM {{ref('stg_jaffle_shop__orders')}} as orders
 left join {{ref('stg_stripe__payments')}} as payments
     using(order_id)
+where payments.payment_status = 'success'
 GROUP BY 1,2
